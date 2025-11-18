4200deb1fdbdfb612a363a4e52c2301c689ab59f
Sirasins <sira001@e.ntu.edu.sg>
2025-11-18 03:28:16 +0000

    1

--- app.py
+++ app.py
@@ -6,8 +6,13 @@ app = Flask(__name__)
 def index():
     return(render_template("index.html"))
 
-if __name__=="__main__":
+@app.route("/",methods=["GET","POST"])
+def main():
+    r = request.form.get("q")
+    return(render_template("main.html"))
 
+if __name__=="__main__":
+    
     app.run()
 
 
